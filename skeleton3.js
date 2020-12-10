30ed91738c9305d2ee35172d1a20ceebb5e46c22
whuck <70232597+whuck@users.noreply.github.com>
2020-12-09 23:28:47 -0600

    card layout, shuffle done ezpz

--- skeleton3.js
+++ skeleton3.js
@@ -4,137 +4,156 @@ var _gameDeck = [
     {
         suit:"c",
         rank:9,
-        img:"card\\c9.png"
+        img:"cards\\c9.png"
     },
     {
         suit:"c",
         rank:10,
-        img:"card\\c10.png"
+        img:"cards\\c10.png"
     },
     {
         suit:"c",
         rank:11,
-        img:"card\\c11.png"
+        img:"cards\\c11.png"
     },
     {
         suit:"c",
         rank:12,
-        img:"card\\c12.png"
+        img:"cards\\c12.png"
     },
     {
         suit:"c",
         rank:13,
-        img:"card\\c13.png"
+        img:"cards\\c13.png"
     },
     {
         suit:"c",
         rank:14,
-        img:"card\\c14.png"
+        img:"cards\\c14.png"
     },
     {
         suit:"s",
         rank:9,
-        img:"card\\s9.png"
+        img:"cards\\s9.png"
     },
     {
         suit:"s",
         rank:10,
-        img:"card\\s10.png"
+        img:"cards\\s10.png"
     },
     {
         suit:"s",
         rank:11,
-        img:"card\\s11.png"
+        img:"cards\\s11.png"
     },
     {
         suit:"s",
         rank:12,
-        img:"card\\s12.png"
+        img:"cards\\s12.png"
     },
     {
         suit:"s",
         rank:13,
-        img:"card\\s13.png"
+        img:"cards\\s13.png"
     },
     {
         suit:"s",
         rank:14,
-        img:"card\\s14.png"
+        img:"cards\\s14.png"
     },
     {
         suit:"h",
         rank:9,
-        img:"card\\h9.png"
+        img:"cards\\h9.png"
     },
     {
         suit:"h",
         rank:10,
-        img:"card\\h10.png"
+        img:"cards\\h10.png"
     },
     {
         suit:"h",
         rank:11,
-        img:"card\\h11.png"
+        img:"cards\\h11.png"
     },
     {
         suit:"h",
         rank:12,
-        img:"card\\h12.png"
+        img:"cards\\h12.png"
     },
     {
         suit:"h",
         rank:13,
-        img:"card\\h13.png"
+        img:"cards\\h13.png"
     },
     {
         suit:"h",
         rank:14,
-        img:"card\\h14.png"
+        img:"cards\\h14.png"
     },
     {
         suit:"d",
         rank:9,
-        img:"card\\d9.png"
+        img:"cards\\d9.png"
     },
     {
         suit:"d",
         rank:10,
-        img:"card\\d10.png"
+        img:"cards\\d10.png"
     },
     {
         suit:"d",
         rank:11,
-        img:"card\\d11.png"
+        img:"cards\\d11.png"
     },
     {
         suit:"d",
         rank:12,
-        img:"card\\d12.png"
+        img:"cards\\d12.png"
     },
     {
         suit:"d",
         rank:13,
-        img:"card\\d13.png"
+        img:"cards\\d13.png"
     },
     {
         suit:"d",
         rank:14,
-        img:"card\\d14.png"
+        img:"cards\\d14.png"
     }
 ];
 
-//arrays for each player's hand
-var p0Hand =[];
-var p1Hand =[];
-
-//array for hands
-var _playerHands = [p0Hand,p1Hand];
-
-//array for player hand div element jquery strings
-var _playerHandDivs = ["#p0Hand","#p1Hand"];
-
-//array for dealerDiv --shows which player is the dealer
-var dealerDivs = ["#p0dealer","#p1dealer"];
+var _player0 ={
+    hand: [],
+    handDiv: 'p0hand',
+    piles: [],
+    pileDivs: ['p0pile0','p0pile1','p0pile2','p0pile3'],
+    dealerDiv: '#p0dealer',
+    trickDiv: 'p0trick'
+};
+var _player1 ={
+    hand: [],
+    handDiv: 'p1hand',
+    piles: [],
+    pileDivs: ['p1pile0','p1pile1','p1pile2','p1pile3'],
+    dealerDiv: '#p1dealer',
+    trickDiv: 'p1trick'
+};
+var _dealOrder = [
+    ['#p1pile0 div.bottom','#p0pile0 div.bottom','#p1pile1 div.bottom','#p0pile1 div.bottom',
+        '#p1pile2 div.bottom','#p0pile2 div.bottom','#p1pile3 div.bottom','#p0pile3 div.bottom',
+        '#p1pile0 div.top','#p0pile0 div.top','#p1pile1 div.top','#p0pile1 div.top',
+        '#p1pile2 div.top','#p0pile2 div.top','#p1pile3 div.top','#p0pile3 div.top',
+        '#p1hand','#p0hand','#p1hand','#p0hand',
+        '#p1hand','#p0hand','#p1hand','#p0hand'],
+    ['#p0pile0 div.bottom','#p1pile0 div.bottom','#p0pile1 div.bottom','#p1pile1 div.bottom',
+        '#p0pile2 div.bottom','#p1pile2 div.bottom','#p0pile3 div.bottom','#p1pile3 div.bottom',
+        '#p0pile0 div.top','#p1pile0 div.top','#p0pile1 div.top','#p1pile1 div.top',
+        '#p0pile2 div.top','#p1pile2 div.top','#p0pile3 div.top','#p1pile3 div.top',
+        '#p0hand','#p1hand','#p0hand','#p1hand',
+        '#p0hand','#p1hand','#p0hand','#p1hand']
+];
+var _players = [_player0,_player1];
 
 //global trump suit
 var _trump = "";
@@ -144,9 +163,54 @@ var trumpCards = [];
 
 //keeps track of dealer
 var _dealer = 0;
+var _currentPlayer =1;
 $(document).ready(function() {
-
+    $("#startGame").click(start);
 });
+function setNextPlayer() {
+    //console.log('next');
+    //$("#p"+_currentPlayer).removeClass('whosTurn');
+    _currentPlayer ++;
+    if(_currentPlayer === 2) _currentPlayer = 0;
+    //$("#p"+_currentPlayer).addClass('whosTurn');
+}
+function start() {
+    var deck = _gameDeck;
+    shuffle(deck);
+    deal(deck);
+}
+function deal(deck) {
+    $(_players[_dealer].dealerDiv).show();
+    for (var i = 0; i < 8; i++) {
+        var c = deck.pop();
+        _players[_currentPlayer].hand.push(c);
+        var cid = `p${_currentPlayer}bot${_players[_currentPlayer].hand.length-1}`;
+        var card = $("<img>").attr('src','cards\\d0.png').attr('id',cid).data('s',c.img);
+        console.log(_dealOrder[_dealer][i]);
+        $(_dealOrder[_dealer][i]).append(card);
+        setNextPlayer();
+    }
+    for (var i = 8; i < 16; i++) {
+        var c = deck.pop();
+        _players[_currentPlayer].hand.push(c);
+        var cid = `p${_currentPlayer}top${_players[_currentPlayer].hand.length-4}`;
+        var card = $("<img>").attr('src',`${c.img}`).attr('id',cid).data('s',c.img);
+        console.log(_dealOrder[_dealer][i]);
+        $(_dealOrder[_dealer][i]).append(card);
+        setNextPlayer();
+    }
+
+    for (var i = 16; i < 24; i++) {
+        var c = deck.pop();
+        _players[_currentPlayer].hand.push(c);
+        var cid = `p${_currentPlayer}hand${_players[_currentPlayer].hand.length-4}`;
+        var card = $("<img>").attr('src',`${c.img}`).attr('id',cid).data('s',c.img);
+        console.log(_dealOrder[_dealer][i]);
+        $(_dealOrder[_dealer][i]).append(card);
+        setNextPlayer();
+    }
+}
+
 //found shuffle function
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
